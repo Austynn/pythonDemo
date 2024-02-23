768ce8e4b6e10f9580b25449a781822e5c982be5
Austynn <austinalex477@gmail.com>
2024-02-23 23:51:17 +0100

    first python commit

---
+++ app.py
@@ -0,0 +1,25 @@
+'''name = input('What is your name? ')
+color = input('What is your favourite color? ')
+print(name + ' likes ' + color )'''
+
+'''birth_year = input('Birth year: ')
+age = 2024 - int(birth_year)
+print(age)'''
+
+'''weight = input('What is you weight: ')
+weight_in_kg = float(weight) / 2.205
+print(weight_in_kg)'''
+
+'''name = 'Jennifer'
+print(name[-5])'''
+
+'''first = 'John'
+last = 'Smith'
+message = first + ' ' + last + ' is a coder'
+msg = f'{first} {last} is a coder'
+print(msg)'''
+
+first_name = 'Steven'
+last_name = 'James'
+message = f'{first_name} {last_name} is a badass developer'
+print(message)
